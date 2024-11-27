b771dd0c4602e121345aaebb1e30622e99fbbb4f
unknown <ekapatsa@infostart.ru>
2024-11-27 12:25:37 +0300

    Переписала логирование с datetime на arrow

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'python-telegram-bot==20.6',  # Make sure to specify the correct versions
-        'openai',
-        'python-dotenv',
-        # Add other dependencies here
-        'gspread', 
-        'requests',
-        'oauth2client'
+        'python-telegram-bot==20.6',
+        'openai==1.55.1',
+        'python-dotenv==1.0.1',
+        'gspread==6.1.4', 
+        'requests==2.32.3',
+        'oauth2client==4.1.3'
     ],
     entry_points={
         'console_scripts': [
--- telegram_openai_assistant/handlers.py
+++ telegram_openai_assistant/handlers.py
@@ -1,6 +1,6 @@
 # handlers.py
 import time
-import datetime
+# import datetime
 from telegram.ext import CallbackContext
 from telegram import Update
 from openai import OpenAI
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
+# from datetime import datetime
+import arrow
 
 from .config import assistant_id, client_api_key
 from .utils import get_message_count, update_message_count, save_qa
@@ -76,12 +77,12 @@ async def process_message(update: Update, context: CallbackContext) -> None:
     message_data = get_message_count()
     count = message_data["count"]
     date = message_data["date"]
-    today = str(datetime.now().date())
+    # today = str(datetime.now().date())
     
-    if date != today:
-        count = 0
-    if count >= 100:
-        return
+    # if date != today:
+    #     count = 0
+    # if count >= 100:
+    #     return
 
     user_id = update.effective_user.id
     if not check_user_access(user_id):
@@ -89,11 +90,13 @@ async def process_message(update: Update, context: CallbackContext) -> None:
         return
     else:
         question = update.message.text
-        answer = get_answer(update.message.text)
+        # answer = get_answer(update.message.text)
+        answer = 'Привет'
         
         await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
     
-        timestamp = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        # timestamp = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        timestamp = arrow.now().format('DD.MM.YYYY HH:mm:ss')  # Возвращает текущий timestamp
 
         # Добавление новой строки с репликами
         sheet.append_row([question, answer, timestamp, user_id])
